47d1bf3d8b0f695523e6acf4f475078d1764293e
OuO333333 <37506309+OuO333333@users.noreply.github.com>
2024-05-13 21:15:51 +0800

    Update EXECUTION.v

--- EXECUTION.v
+++ EXECUTION.v
@@ -14,45 +14,38 @@ module EXECUTION(
 
 input clk,rst,ALUop;
 input [31:0] A,B;
-input [4:0]DX_RD;
+input [4:0] DX_RD;
 input [2:0] ALUctr;
 
-output reg [31:0]ALUout;
-output reg [4:0]XM_RD;
+output reg [31:0] ALUout;
+output reg [4:0] XM_RD;
 
 //set pipeline register
 always @(posedge clk or posedge rst)
 begin
-  if(rst)
-    begin
-	  XM_RD	<= 5'd0;
-	end 
-  else 
-	begin
-	  XM_RD <= DX_RD;
-	end
+    if(rst)begin
+        XM_RD <= 5'd0;
+    end else begin
+        XM_RD <= DX_RD;
+    end
 end
 
 // calculating ALUout
 always @(posedge clk or posedge rst)
 begin
-  if(rst)
-    begin
-	  ALUout	<= 32'd0;
-	end 
-  else 
-	begin
-	  case(ALUctr)
-	    3'd0: //add //lw //sw
-		  begin
-	        ALUout <=A+B;
-		  end
-		3'd1: //sub
-		  begin
-		    //define sub behavior here
-		  end
-	  endcase
-	end
+    if(rst)begin
+        ALUout <= 32'd0;
+    end else begin
+        case(ALUctr)
+	    3'd0: begin
+                //add //lw //sw
+	        ALUout <= A + B;
+            end
+            3'd1: begin
+                //define sub behavior here
+                ALUout <= A - B;
+            end
+        endcase
+    end
 end
 endmodule
-	
