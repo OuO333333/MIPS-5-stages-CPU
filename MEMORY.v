945eaf1f99576d1ab844007e6962854a539a66c6
OuO333333 <37506309+OuO333333@users.noreply.github.com>
2024-05-13 21:44:04 +0800

    Update MEMORY.v

--- MEMORY.v
+++ MEMORY.v
@@ -19,25 +19,22 @@ output reg [4:0] MW_RD;
 //data memory
 reg [31:0] DM [0:127];
 /*
-//always store word to data memory
-always @(posedge clk)
-  if(XM_MemWrite)
-    DM[ALUout[6:0]] <= XM_MD;
+  always store word to data memory
+  always @(posedge clk)
+      if(XM_MemWrite)
+          DM[ALUout[6:0]] <= XM_MD;
 */
 
-//send to Dst REG: "load word from data memory" or  "ALUout"
+// Send to Dst REG: "load word from data memory" or  "ALUout"
 always @(posedge clk)
 begin
-  if(rst)
-    begin
-	  MW_ALUout	<=	32'b0;
-	  MW_RD		<=	5'b0;
-	end
-  else
-    begin
-	  MW_ALUout	<=	ALUout;
-	  MW_RD		<=	XM_RD;
-	end
+    if(rst)begin
+            MW_ALUout <= 32'b0;
+            MW_RD <= 5'b0;
+    end else begin
+        MW_ALUout <= ALUout;
+        MW_RD <= XM_RD;
+    end
 end
 
 endmodule
